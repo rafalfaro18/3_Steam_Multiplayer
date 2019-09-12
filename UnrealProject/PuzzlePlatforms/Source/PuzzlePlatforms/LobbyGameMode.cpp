73397eea768697ec16ea55826c6d8705d63a1d29
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-09-11 23:49:59 -0600

    Fixes spawn bug

--- UnrealProject/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ UnrealProject/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -4,6 +4,7 @@
 #include "LobbyGameMode.h"
 
 void ALobbyGameMode::PostLogin(APlayerController * NewPlayer) {
+	Super::PostLogin(NewPlayer);
 	++NumberOfPlayers;
 
 	if (NumberOfPlayers >= 3) {
@@ -12,5 +13,6 @@ void ALobbyGameMode::PostLogin(APlayerController * NewPlayer) {
 }
 
 void ALobbyGameMode::Logout(AController * Exiting) {
+	Super::Logout(Exiting);
 	--NumberOfPlayers;
 }
